2cfcf115a494b5484cd7b086e7b1e8fd4a426dcd
J-A-S-H-U <89767963+J-A-S-H-U@users.noreply.github.com>
2025-02-06 19:41:02 -0800

    Updated UVM TB

--- UVM_TB/fpu_environment.sv
+++ UVM_TB/fpu_environment.sv
@@ -8,25 +8,25 @@ class fpu_env extends uvm_env;
 
     function new(string name="fpu_env",uvm_component parent);
         super.new(name,parent);
-        `uvm_info("fpu_env", "Inside constructor of fpu_env", UVM_HIGH)
+        `uvm_info("fpu_env", "Inside constructor", UVM_HIGH)
     endfunction
 
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        `uvm_info(get_name(), "Inside build phase", UVM_HIGH)
+        `uvm_info("fpu_env", "Inside build phase", UVM_HIGH)
       	agent=fpu_agent::type_id::create("agent",this);
     	scb=fpu_scoreboard::type_id::create("scb",this);
     endfunction: build_phase
 
     function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
-        `uvm_info(get_name(), "Inside connect phase", UVM_HIGH)
+        `uvm_info("fpu_env", "Inside connect phase", UVM_HIGH)
       agent.mon.mon_port.connect(scb.scb_port);
     endfunction
 
     task run_phase(uvm_phase phase);
         super.run_phase(phase);
-        `uvm_info(get_name(), "Inside run phase", UVM_HIGH)
+        `uvm_info("fpu_env", "Inside run phase", UVM_HIGH)
     endtask
 
 endclass: fpu_env
