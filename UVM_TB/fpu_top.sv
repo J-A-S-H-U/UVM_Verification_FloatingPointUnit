8d7b77704f3863ab8220780052d00ceda22410c4
J-A-S-H-U <89767963+J-A-S-H-U@users.noreply.github.com>
2025-02-06 19:53:47 -0800

    Updated UVM TB

--- UVM_TB/fpu_top.sv
+++ UVM_TB/fpu_top.sv
@@ -1,6 +1,7 @@
 `timescale 1ns/1ns
 
 `include "fpu_def.sv"
+
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 
@@ -12,23 +13,20 @@ module top;
     fpu_if top_if(clk);
 
   fpu_top_RTL dut (
-    	.clk      (top_if.clk      ),
-      .reset    (top_if.reset    ),
-      .din1     (top_if.din1     ),
+    	.clk(top_if.clk),
+      .reset(top_if.reset),
+      .din1(top_if.din1),
       .cmd(top_if.cmd),
-      .din2     (top_if.din2     ),
-      .valid     (top_if.valid     ),
-
-      .result   (top_if.result   ),
-      .ready      (top_if.ready      )
-  );
+      .din2(top_if.din2),
+      .valid(top_if.valid),
+      .result(top_if.result),
+      .ready(top_if.ready));
 
 //clock generation
     initial forever #1 clk=~clk;
 
  	initial begin
       uvm_config_db #(virtual fpu_if) :: set(null,"*","fpu_vif",top_if);
-      `uvm_info("TOP","Configured database for interface...",UVM_LOW)
     end
 
     initial begin
@@ -41,4 +39,3 @@ module top;
   end
 
 endmodule
-// TODO: Receiving items from monitor in scoreboard
