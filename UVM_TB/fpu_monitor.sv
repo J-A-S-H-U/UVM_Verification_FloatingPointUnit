aef4ec69a8bdb15110a90b8514f45c1d984ef542
J-A-S-H-U <89767963+J-A-S-H-U@users.noreply.github.com>
2025-02-06 19:41:41 -0800

    Updated UVM TB

--- UVM_TB/fpu_monitor.sv
+++ UVM_TB/fpu_monitor.sv
@@ -4,54 +4,53 @@ import uvm_pkg::*;
 class fpu_monitor extends uvm_monitor;
   `uvm_component_utils(fpu_monitor)
   virtual fpu_if vif;
-  fpu_sequence_item item;
+  fpu_sequence_item mon_pkt;
   
   uvm_analysis_port #(fpu_sequence_item) mon_port;
   
   function new(string name="fpu_monitor",uvm_component parent);
       super.new(name,parent);
-    `uvm_info("fpu_monitor", "Inside constructor of fpu_monitor", UVM_HIGH)
+    `uvm_info("fpu_monitor", "Inside constructor", UVM_HIGH)
   endfunction
 
   function void build_phase(uvm_phase phase);
       super.build_phase(phase);
-      `uvm_info(get_name(), "Inside build phase", UVM_HIGH)
+      `uvm_info("fpu_monitor", "Inside build phase", UVM_HIGH)
     
     mon_port=new("mon_port",this);
 
       if(!(uvm_config_db #(virtual fpu_if)::get(this,"*","fpu_vif",vif)))
-        `uvm_error(get_name(), "Faild to get Virtual IF from database...")  
+        `uvm_error("fpu_monitor", "Faild to get Virtual Interface")  
         
   endfunction: build_phase
 
   function void connect_phase(uvm_phase phase);
       super.connect_phase(phase);
-      `uvm_info(get_name(), "Inside connect phase", UVM_HIGH)
+      `uvm_info("fpu_monitor", "Inside connect phase", UVM_HIGH)
   endfunction
 
   task run_phase(uvm_phase phase);
       super.run_phase(phase);
-      `uvm_info(get_name(), "Inside run phase", UVM_HIGH)
+      `uvm_info("fpu_monitor", "Inside run phase", UVM_HIGH)
       forever begin
-        item=fpu_sequence_item::type_id::create("item");
-        sample(item);
-        `uvm_info(get_name(),"Item received!!",UVM_HIGH)
-        mon_port.write(item);
+        mon_pkt=fpu_sequence_item::type_id::create("mon_pkt");
+        sample(mon_pkt);
+        mon_port.write(mon_pkt);
       end
   endtask
 
 
-  task sample(fpu_sequence_item item);
+  task sample(fpu_sequence_item mon_item);
 
     @(posedge vif.ready);
 
-    item.cmd=vif.cmd;
-    item.din1=vif.din1;
-    item.din2=vif.din2;
-    item.result=vif.result;
-    item.reset=vif.reset;
-    item.valid=vif.valid;
-    item.ready=vif.ready;
+    mon_item.cmd=vif.cmd;
+    mon_item.din1=vif.din1;
+    mon_item.din2=vif.din2;
+    mon_item.result=vif.result;
+    mon_item.reset=vif.reset;
+    mon_item.valid=vif.valid;
+    mon_item.ready=vif.ready;
   endtask
 
 endclass: fpu_monitor
